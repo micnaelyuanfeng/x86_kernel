6b237ec0fdd601e2ec6c609c02745747ca5c89ec
michaelyuanfeng <michaelyuan.feng@mail.utoronto.ca>
2018-05-08 14:43:54 -0400

    GDT test

---
+++ ass2/types.h
@@ -0,0 +1,9 @@
+#ifndef _TYPES_H_
+#define _TYPES_H_
+
+typedef unsigned char  uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int   uint32_t;
+typedef unsigned long  uint64_t;
+
+#endif
