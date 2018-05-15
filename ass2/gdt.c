ca49fa330208306b99b821622b1eb3fb42ddf194
michaelyuanfeng <michaelyuan.feng@mail.utoronto.ca>
2018-05-15 16:26:50 -0400

    Add files via upload

--- ass2/gdt.c
+++ ass2/gdt.c
@@ -1,13 +1,13 @@
 #include "types.h"
 #include "gdt.h"
 
-void Display_Msg(void);
+extern void Display_Msg(void);
 
-seg_desc_t gdt[3] = {
+seg_desc_t gdt[4] = {
     [0] = SEG_DESC(0,       0,      0,            0,              0,     0,           0,       0),
     [1] = SEG_DESC(0x0,     0xff,   TYPE_CODE_XO, DESC_TYPE_CODE, DPL_0, SEG_PRESENT, CODE_32, G_BYTE),
-    [3] = SEG_DESC(0x0,     0xfff,  TYPE_DATA_RW, DESC_TYPE_DATA, DPL_0, SEG_PRESENT, DATA_32, G_BYTE),
-    [2] = SEG_DESC(0xb8000, 0xffff, TYPE_DATA_RW, DESC_TYPE_DATA, DPL_0, SEG_PRESENT, DATA_32, G_BYTE),
+    [2] = SEG_DESC(0x0,     0xfff,  TYPE_DATA_RW, DESC_TYPE_DATA, DPL_0, SEG_PRESENT, DATA_32, G_BYTE),
+    [3] = SEG_DESC(0xb8000, 0xffff, TYPE_DATA_RW, DESC_TYPE_DATA, DPL_0, SEG_PRESENT, DATA_32, G_BYTE),
 };
 
 gdtr_t gdtr = {
@@ -17,6 +17,6 @@ gdtr_t gdtr = {
 
 void gdt_init(void)
 {
-	gdt[1].base_23_0  = Display_Msg & 0xFFFFFF;
-	gdt[1].base_31_24 = (Display_Msg >> 24) & 0xFF; 
+	gdt[1].base_23_0  = 0xC6100 + (uint32_t)Display_Msg;
+	gdt[1].base_31_24 = 0x0; 
 }
--- ass2/types.h
+++ ass2/types.h
@@ -4,7 +4,7 @@
 typedef unsigned char  uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int   uint32_t;
-typedef unsigned long  uint64_t;
+//typedef unsigned long  uint64_t;
 
 #define NULL				0
 #define false				0
