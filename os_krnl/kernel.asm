92bf4f4f748b63c032236abd02d6dacf5488c399
michaelyuanfeng <michaelyuan.feng@mail.utoronto.ca>
2018-06-12 07:50:38 -0400

    Add files via upload

--- os_krnl/kernel.asm
+++ os_krnl/kernel.asm
@@ -1,28 +1,40 @@
 [SECTION .text]	
-
+SECLETOR_KERNEL_CS			equ 0x8
+extern cstart; 
+extern gdtPtr
+ 
 global _kstart	
 
 _kstart:
 	mov ebp, kernelStackTop 
 	mov esp, kernelStackTop
 
-	sgdt [oldgdtPtrPhyAddr]
+	sgdt [gdtPtr]
 
-	push dword gdtPhyAddr
-	push dword oldgdtPtrPhyAddr
-	push dword gdtPhyAddr + 2
+	;push dword gdtPhyAddr
+	;push dword oldgdtPtrPhyAddr
+	;push dword gdtPhyAddr + 2
 
-	call MemCopy
+	call cstart
 
-	add esp, 0xC
-
-	lgdt [gdtPhyAddr]
+	;add esp, 0xC
+  
+;error note -> not defined memCpu
+;           -> compile execced range of 16 bit mode	 
+;		    -> data type cast
+;           ->
+	lgdt [gdtPtr]
 
 	mov	ah, 0x0A				 
 	mov	al, 'P'
-	mov	[gs:((80 * 1 + 39) * 2)], ax
+	mov	[gs:((80 * 20 + 0) * 2)], ax
 
-	jmp	$
+	jmp SECLETOR_KERNEL_CS:csinit
+
+csinit:
+	jmp $
+
+global MemCopy
 
 MemCopy:
 	push ebp	 ;save caller ebp
@@ -31,7 +43,6 @@ MemCopy:
 	push esi     ; index registers
 	push edi     ; callee save for self using
 	push ecx
-	push eax
 
 	mov  edi, [ebp + 8]
 	mov  esi, [ebp + 12]
@@ -53,7 +64,6 @@ MemCopy:
 .cpyDone:
 	mov eax, [ebp + 8] ;eax for return value = convention
 
-	pop eax
 	pop ecx
 	pop edi
 	pop esi
@@ -66,27 +76,3 @@ MemCopy:
 kernelStack				resb		2048
 kernelStackTop			equ			$
 
-[SECTION .gdt]
-oldGdtPtr:
-	times         6 	db      0
-oldgdtPtrPhyAddr		equ     KernelEntryPhyAddr + oldGdtPtr
-
-[SECTION .data]
-%include "pm.inc"
-
-KernelEntryPhyAddr		equ		0x030400 
-
-GDT_TABLE_START:
-GDT_DEFAULT:	Descriptor			0,			0,					0		
-GDT_CODE_SYS:   Descriptor     		0,	  		0xFFFFF,			DA_CR|DA_32|DA_LIMIT_4K 
-GDT_DATA_SYS:	Descriptor			0,	  		0xFFFFF,			DA_DRW|DA_32|DA_LIMIT_4K ;Data Segment Read and Write
-GDT_VIDEO:		Descriptor	  		0xB8000,    0xFFFF,          	DA_DRW|DA_DPL3 ;Data Segment Read and Write
-
-selectorCode		equ		GDT_CODE_SYS  - GDT_DEFAULT
-selectorData		equ     GDT_DATA_SYS  - GDT_DEFAULT
-selectorVideo       equ     GDT_VIDEO     - GDT_DEFAULT   + SA_RPL3
-
-gdtLen				equ		$ - GDT_TABLE_START
-gdtPtr				dw		gdtLen - 1 
-					dd      KernelEntryPhyAddr + GDT_DEFAULT
-gdtPhyAddr			equ		KernelEntryPhyAddr + gdtPtr
--- os_krnl/types.h
+++ os_krnl/types.h
@@ -1,9 +1,9 @@
 #ifndef _TYPES_H_
 #define _TYPES_H_
 
-#define unsigned char  uint8_t
-#define unsigned short uint16_t
-#define unsigned int   uint32_t
+typedef unsigned char  uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int   uint32_t;
 
 #define NULL	0
 #define true    1
