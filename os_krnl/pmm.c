517d407dc70ddde1c9361ca20150a05b48b83618
michaelyuanfeng <michaelyuan.feng@mail.utoronto.ca>
2018-06-13 16:51:55 -0400

    Update pmm.c

--- os_krnl/pmm.c
+++ os_krnl/pmm.c
@@ -1,1 +1,40 @@
 
+#include "types.h"
+#include "debug.h"
+#include "pmm.h"
+
+#define NUM_MEM_SEG_MAX   32
+
+mem_seg_entry_t[NUM_MEM_SEG_MAX];
+
+bool init_mem_seg_entry()
+{
+  uint32_t i = 0;
+  
+  for(; i < NUM_MEM_SEG_MAX; i++)
+  {
+    mem_seg_entry_t[i].size          = 0xFFFFFFFF;
+    mem_seg_entry_t[i].base_addr_low = 0xFFFFFFFF;
+    mem_seg_entry_t[i].base_addr_hi  = 0xFFFFFFFF;
+    mem_seg_entry_t[i].length_low    = 0xFFFFFFFF;
+    mem_seg_entry_t[i].length_hi     = 0xFFFFFFFF;
+    mem_seg_entry_t[i].type          = 0xFFFFFFFF;
+  }
+  
+  return true;
+}
+
+bool copy_mem_seg_entry()
+{
+  uint32_t effective_segs = 0;
+  uint32_t seg_index      = 0;
+  
+  if(get_num_mem_s(&effective_segs) != 0)
+  {
+    MemCopy(mem_seg_entry_t, effective_segs*sizeof(mem_seg_entry_t));
+  }
+  else
+  {
+    debug_print();
+  }
+}
