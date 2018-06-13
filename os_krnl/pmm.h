cb32993101e4ae260e62458391e3994c4abd4516
michaelyuanfeng <michaelyuan.feng@mail.utoronto.ca>
2018-06-13 16:34:58 -0400

    Update pmm.h

--- os_krnl/pmm.h
+++ os_krnl/pmm.h
@@ -1,1 +1,8 @@
-typedef 
+typedef struct memory_segment
+{
+  uint32_t size;
+  uint32_t base_addr_low;
+  uint32_t base_add_high;
+  uint32_t length_low;
+  uint32_t length_high;
+}__attribute__((packed))mem_seg_entry_t;
