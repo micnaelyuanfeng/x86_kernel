bff43707deb948fc3edf7643be466599b43ff55f
michaelyuanfeng <michaelyuan.feng@mail.utoronto.ca>
2018-06-22 15:42:31 -0400

    Add files via upload

---
+++ testKnrl/intr_switch.asm
@@ -0,0 +1,95 @@
+[SECTION .interrupt_entry]
+; Interrupt Service Rotine
+; These handlers are used for
+; Software Interrupts
+
+extern isr_handler_dispatcher
+
+global isr_ctx_save
+
+isr_ctx_save:
+	pusha
+	mov ax, ds
+	push eax
+
+	mov ax, 0x10
+	mov ds, ax
+	mov es, ax
+	mov fs, ax
+	mov gs, ax
+	mov ss, ax
+
+	push esp
+	call isr_handler_dispatcher
+	add esp, 0x4
+
+	pop ebx
+	mov ds, bx
+	mov es, bx
+	mov fs, bx
+	mov gs, bx
+	mov ss, bx
+
+	popa
+	add esp, 0x8
+	iret
+
+%macro ISR_NOERRCODE	1
+global isr%1
+
+isr%1:
+	cli
+	push 0				 ;Push Invalid error code = consistent
+	push %1
+	jmp  isr_ctx_save
+
+%endmacro
+
+%macro ISR_ERRCODE    1
+global isr%1
+
+isr%1:
+	cli
+	push %1               ;CPU will push err code
+	jmp  isr_ctx_save
+%endmacro
+
+ISR_NOERRCODE 		0
+ISR_NOERRCODE 		1
+ISR_NOERRCODE 		2
+ISR_NOERRCODE 		3
+ISR_NOERRCODE 		4
+ISR_NOERRCODE 		5
+ISR_NOERRCODE 		6
+ISR_NOERRCODE 		7
+ISR_ERRCODE 		8
+ISR_NOERRCODE 		9
+ISR_ERRCODE 		10
+ISR_ERRCODE 		11
+ISR_ERRCODE 		12
+ISR_ERRCODE 		13
+ISR_ERRCODE 		14
+ISR_NOERRCODE 		15
+ISR_NOERRCODE 		16
+ISR_ERRCODE 		17
+ISR_NOERRCODE 		18
+ISR_NOERRCODE 		19
+ISR_NOERRCODE 		20
+ISR_NOERRCODE 		21
+ISR_NOERRCODE 		22
+ISR_NOERRCODE 		23
+ISR_NOERRCODE 		24
+ISR_NOERRCODE 		25
+ISR_NOERRCODE 		26
+ISR_NOERRCODE 		27
+ISR_NOERRCODE 		28
+ISR_NOERRCODE 		29
+ISR_NOERRCODE 		30
+ISR_NOERRCODE 		31
+
+ISR_NOERRCODE 		255
+
+
+; Interrupt Request Rotine
+; These handlers are used for
+; HardwareInterrupts
---
+++ testKnrl/kernel.asm
@@ -0,0 +1,90 @@
+[SECTION .text]	
+SECLETOR_KERNEL_CS			equ 0x8
+extern cstart; 
+extern gdtPtr
+extern idtPtr;
+
+extern intr_test;
+ 
+global _kstart	
+
+_kstart:
+	;mov ebp, kernelStackTop 
+	mov esp, kernelStackTop
+
+	mov	ah, 0x0A				 
+	mov	al, 'P'
+	mov	[gs:((80 * 20 + 0) * 2)], ax
+ 
+	jmp SECLETOR_KERNEL_CS:csinit
+ 
+	sgdt [gdtPtr]
+
+	;push dword gdtPhyAddr
+	;push dword oldgdtPtrPhyAddr
+	;push dword gdtPhyAddr + 2  
+
+	call cstart
+
+	;add esp, 0xC
+  
+;error note -> not defined memCpu
+;           -> compile execced range of 16 bit mode	 
+;		    -> data type cast
+;           ->
+	lgdt [gdtPtr]
+	lidt [idtPtr]
+
+	mov	ah, 0x0A				 
+	mov	al, 'P'
+	mov	[gs:((80 * 20 + 0) * 2)], ax
+
+	call intr_test
+
+	jmp SECLETOR_KERNEL_CS:csinit
+
+csinit:
+	jmp $
+
+global MemCopy
+
+MemCopy:
+	push ebp	 ;save caller ebp
+	mov  ebp, esp ;current caller esp is callee ebp
+
+	push esi     ; index registers
+	push edi     ; callee save for self using
+	push ecx
+
+	mov  edi, [ebp + 8]
+	mov  esi, [ebp + 12]
+	mov  ecx, [ebp + 16]
+
+.cpyGoOn:
+	cmp ecx, 0
+	jz  .cpyDone
+
+	mov al, [ds:esi]
+	inc esi
+
+	mov byte [es:edi], al
+	inc edi
+
+	dec ecx
+	jmp .cpyGoOn
+
+.cpyDone:
+	mov eax, [ebp + 8] ;eax for return value = convention
+
+	pop ecx
+	pop edi
+	pop esi
+	mov esp, ebp
+	pop ebp
+
+	ret 
+
+[SECTION .bss]
+kernelStack				resb		2048
+kernelStackTop			equ			$
+
