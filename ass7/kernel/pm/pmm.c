96ec702ad524af459fe735b7d5dff2062dee8772
michaelyuanfeng <michaelyuan.feng@mail.utoronto.ca>
2018-08-05 10:08:53 -0400

    Add files via upload

--- ass7/kernel/pm/pmm.c
+++ ass7/kernel/pm/pmm.c
@@ -1,100 +1,88 @@
-#include "types.h"
-#include "display.h"
 #include "pmm.h"
 
-void* heap_list = NULL;
 
-/*
-	Aussume largest allocated chunk size = 4KB
-	Any chunk > 4KB will be put in the last
-	list of the array
-*/
-list_entry_t* free_list[12];
-
-void mm_init(void)
+/************************************************
+	Free Block Combine Functions
+	-> both neighbours are allocated
+	-> next block is free = can be combined
+	-> prev block is free = can be combined
+	-> next and prev block are free and can be combined
+*************************************************/
+void* coalesce(void* this_block)
 {
-	
-}
+	uint32_t prev_block_allocated = GET_ALLOC(FTRP(PREV_BLKP(this_block)));
+	uint32_t next_block_allocated = GET_ALLOC(HDRP(NEXT_BLKP(this_block)));
+	uint32_t size                 = GET_SIZE(HDRP(this_block));
 
-void* mm_malloc(uint32_t size)
-{
-
-}
-
-void* mm_realloc(void* this_mem_ptr, uint32_t size)
-{
-	
-}
-
-void* mm_free(void* this_mem_ptr)
-{
-	
-}
-
-void* mm_coalesce(void* this_mem_ptr)
-{
-	
-}
-
-/*
-	This Function find the corresponding link list
-	for the input size
-*/
-void* mm_search_fit(uint32_t alloc_size)
-{
-	if(alloc_size == 0)
+	if(prev_block_allocated && next_block_allocated)
 	{
-		return NULL;
+		return this_block;
 	}
-	
-	uint32_t free_list_index   = list_hash(alloc_size);
-	uint8_t  list_index        = 0;
-	uint32_t block_size        = 0;
-	uint32_t remaining_size	   = 0;
-	
-	list_entry_t* list_ptr      = NULL;
-	list_entry_t* traverse_ptr  = NULL;
-	list_entry_t* list_elem_ptr = NULL;
-	list_entry_t* remaining_ptr = NULL:
-	list_entry_t* alloc_ptr     = NULL;
-	
-	//Using First Fit Algo
-	for(list_index = free_list_index; list_index < 12; list_index++)
+	else if(prev_block_allocated && !next_block_allocated)
 	{
-		if(free_list[list_index] != NULL)
-		{
-			//Get List Head
-			list_elem_ptr = free_list[list_index];
-			
-			//Get Block Information
-			block_size = GET_SIZE(HDRP(list_elem_ptr));
-			
-			//Situation 1
-			if(block_size >= alloc_size && (block_size - alloc_size) >= MIN_SIZE)
-			{
-				list_del(list_elem_ptr);
-				remaining_size = block_size - alloc_size;
-				
-				alloc_ptr = (uint8_t*)list_elem_ptr + remaining_size;
-				
-				//Mark allocated chunck information
-				PUT(HDRP(alloc_ptr), PACK(alloc_size, 1));
-				PUT(FTRP(alloc_ptr), PACK(alloc_size, 1));
-				
-				//Mark leftover chunk information
-				PUT(HDRP(list_elem_ptr), PACK(remaining_size, 1));
-				PUT(FTRP(list_elem_ptr), PACK(remaining_size, 1));
-			}
-			
-			//Update the free list
-			if(remaining_size > 0)
-			{
-			}
-		}
+		list_entry_t* next = (list_entry_t*)NEXT_BLKP(this_block);
+		list_del(next);
+		
+		size += GET_SIZE(HDRP(NEXT_BLKP(this_block)));
+		
+		PUT(HDRP(this_block), PACK(size, 0));
+		PUT(FTRP(this_block), PACK(size, 0));
+		
+		return this_block;
+	}
+	else if(!prev_block_allocated && next_block_allocated)
+	{
+		list_entry_t* prev = (list_entry_t*)PREV_BLKP(this_block);
+		list_del(prev);
+		
+		size += GET_SIZE(HDRP(PREV_BLKP)(this_block)));
+		
+		PUT(FTRP(this_block), PACK(size, 0));
+		PUT(HDRP(PREV_BLKP(this_block)), PACK(size, 0));
+		
+		return (PREV_BLKP(this_block));
+	}
+	else
+	{
+		list_entry_t* next = (list_entry_t*)NEXT_BLKP(this_block);
+		list_entry_t* prev = (list_entry_t*)PREV_BLKP(this_block);
+		
+		size += GET_SIZE(HDRP(NEXT_BLKP(this_block)));
+		size += GET_SIZE(HDRP(PREV_BLKP)(this_block)));
+		
+		list_del(prev);
+		list_del(next);
+		
+		PUT(HDRP(PREV_BLKP(this_block)), PACK(size, 0));
+		PUT(HDRP(NEXT_BLKP(this_block)), PACK(size, 0));
+	
+		reuturn (PREV_BLKP(this_block));
 	}
 }
 
-void  mm_mark_used(void* this_mem_ptr, uint32_t size)
+void* extend_heap()
 {
+	uint8_t* block_ptr = NULL;
+	uint32_t size      = PAGE_SIZE;
 	
-}	
+	block_ptr = heap_stack[heap_index];
+	
+	PUT(HDRP(block_ptr), PAGE_SIZE(size, 0));
+	PUT(FTRP(block_ptr), PAGE_SIZE(size, 0));
+	
+	list_add(block_ptr);
+	
+	return (void*)block_ptr;
+}
+
+bool mm_init()
+{
+	//Initialize free list
+	for(uint8_t* i = 0; i < 12; i++)
+	{
+		free_list[i] = NULL;
+	}
+	
+	return 0;
+}
+
